8859909d949150ab237930012967e240422c3942
itsRews <141185898+itsRews@users.noreply.github.com>
2024-02-23 22:16:17 +0100

    Create NubesGlobalAddonScript.js

---
+++ standalone/NubesGlobalAddonScript.js
@@ -0,0 +1,40 @@
+async function waitForAllInit() {
+    return new Promise(resolve => {
+        const intervalId = setInterval(() => {
+            if (typeof Engine !== 'undefined' && typeof Engine.hero !== 'undefined') {
+                if (Engine.allInit === true) {
+                    clearInterval(intervalId);
+                    resolve();
+                }
+            }
+            else {
+                if (g.init === 5) {
+                    setInterval(() => {
+                        clearInterval(intervalId);
+                        resolve();
+                    }, 100);
+                }
+            }
+        }, 100);
+    });
+}
+
+(async () => {
+    await waitForAllInit();
+    if (typeof Engine !== 'undefined' && typeof Engine.hero !== 'undefined') {
+      if (window.dad.config.get("lootlog")) {
+        window.dad.config.set("lootlog", false)
+        mAlert(`Automatycznie wyłączono zapisywanie lootów w Dodatku Globalnym. Chcesz odświeżyć grę teraz, czy  później?`, [{
+                txt: "Teraz",
+                callback: function() {
+                    window.pageReload();
+                }
+            }, {
+                txt: "Później",
+                callback: function() {
+                    return !0
+                }
+            }])
+      }
+    }
+})();
